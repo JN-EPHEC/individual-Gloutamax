2979a6e8541ccdd2493864654992a686b9b13a1b
Justin DAUB <justin.daub.fr@gmail.com>
2026-02-05 15:07:10 +0100

    TP1: Partie 5 Création d'un serveur express.js

--- dev3-backend-tp1/src/server.ts
+++ dev3-backend-tp1/src/server.ts
@@ -1,6 +1,12 @@
-function greet(name: string): string {
-    return `Bonjour ${name}`;
-}; 
+import express, {Application, Request, Response} from 'express'; 
 
-console.log(greet("Justin"));
+const app: Application = express(); 
+const port = 3000; 
 
+app.get('/', (req: Request, res: Response) => {
+    res.send("Bienvenue sur mon serveur API.");
+});
+
+app.listen(port, () => {
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+});
