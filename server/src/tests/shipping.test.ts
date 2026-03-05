c21bbda9a857af535b4bf9f1d85479368e6584d2
Gloutamax <justin.daub.fr@gmail.com>
2026-03-05 15:13:18 +0100

    TP4: 5.2 Le N-Wise Testing

--- server/src/tests/shipping.test.ts
+++ server/src/tests/shipping.test.ts
@@ -33,4 +33,39 @@ describe('Shipping Calculator - Tests Fonctionnels', () => {
             }
         );
     });
+
+    describe('2. Pairwise Combinations', () => {
+        const pairwiseCases = [
+            // ID 1 : D1 (Courte), W1 (Léger), T1 (Standard)
+            // Calcul : Bases 10€ + 0% = 10€
+            { desc: 'Scénario : 25 km, 5 kg, standard -> Total 10 €', distance: 25, weight: 5, type: 'standard', expected: 10},
+
+            // ID 2 : D1 (Courte), W2 (Lourd), T2 (Express)
+            // Calcul : (Base 10€ + 50%) * 2 = 15€ * 2 = 30€
+            { desc: 'Scénario : 25 km, 20 kg, express -> Total 30 €', distance: 25, weight: 20, type: 'express', expected: 30 },
+
+            // ID 3 : D2 (Moyenne), W1 (Léger), T2 (Express)
+            // Calcul : (Base 25€ + 0%) * 2 = 50€
+            { desc: 'Scénario : 100 km, 5 kg, express -> Total 50 €', distance: 100, weight: 5, type: 'express', expected: 50},
+
+            // ID 4 : D2 (Moyenne), W2 (Lourd), T1 (Standard)
+            // Calcul : Base 25€ + 50% = 37.5 €
+            { desc: 'Scénario : 100 km, 20 kg, standard -> Total 37.5 €', distance: 100, weight: 20, type: 'standard', expected: 37.5},
+
+            // ID 5 : D3 (Longue), W1 (Léger), T2 (Express)
+            // Calcul : (Base 50€ + 0%) * 2 = 100€
+            { desc: 'Scénario : 600 km, 5 kg, express -> Total 100 €', distance: 600, weight: 5, type: 'express', expected: 100},
+
+            // ID 6 : D3 (Longue), W2 (Lourd), T1 (Standard)
+            // Calcul : Base 50€ + 50% = 75€
+            { desc: 'Scénario : 600 km, 20 kg, standard -> Total 75 €', distance: 600, weight: 20, type: 'standard', expected: 75},
+        ];
+
+        test.each(pairwiseCases)(
+            '$desc',
+            ({ distance, weight, type, expected }) => {
+                expect(calculateShipping(distance, weight, type as any)).toBe(expected);
+            }
+        );
+    });
 });
